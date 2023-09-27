ffabb812dea3c8b1277da790e8306d7f544ed37d
dlopes <dlopes@gasolina>
2023-09-27 22:10:02 +0100

    fixed paths on E3

--- experiment3.sh
+++ experiment3.sh
@@ -1,6 +1,6 @@
 cd sumo_pipeline/source_separation
-python3 app.py test-full-pipeline OSTest /mnt/nas-shared/torpedo/extracted_features/extracted_features_OSTest/stats_source_separation.csv source_separation_model_bayesian_optimization.joblib
+python3 app.py test-full-pipeline OSTest ../../extracted_features/extracted_features_OSTest/stats_source_separation.csv ../../models/source_separation/source_separation_model_bayesian_optimization.joblib
 cd ../target_separation
-python3 app.py test-full-pipeline OSTest target_separation_model_bayesian_optimization.joblib
+python3 app.py test-full-pipeline OSTest ../../models/target_separation/target_separation_model_bayesian_optimization.joblib
 cd ../session_correlation
 python3 app.py correlate-sessions-full-pipeline OSTest
