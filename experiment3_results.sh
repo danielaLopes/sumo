737e174e3aba3944a218545bc059f7b25f2e4330
dlopes <dlopes@gasolina>
2023-09-27 22:11:36 +0100

    fixed paths on E3

--- experiment3_results.sh
+++ experiment3_results.sh
@@ -1,6 +1,6 @@
 cd sumo_pipeline/source_separation
-python3 app.py plot-validate ../../extracted_features/extracted_features_OSTest/stats_source_separation.csv ../../models/source_separation_model_bayesian_optimization.joblib precision_recall_curve_source_separation_hyperparameter_tuning_zoomin
+python3 app.py plot-validate ../../extracted_features/extracted_features_OSTest/stats_source_separation.csv ../../models/source_separation/source_separation_model_bayesian_optimization.joblib precision_recall_curve_source_separation_hyperparameter_tuning_zoomin
 cd ../target_separation
-python3 app.py plot-validate ../../extracted_features/extracted_features_OSTest/stats_target_separation.csv ../../models/target_separation_model_bayesian_optimization.joblib precision_recall_curve_target_separation_hyperparameter_tuning_zoomin
+python3 app.py plot-validate ../../extracted_features/extracted_features_OSTest/stats_target_separation.csv ../../models/target_separation/target_separation_model_bayesian_optimization.joblib precision_recall_curve_target_separation_hyperparameter_tuning_zoomin
 cd ../session_correlation
 python3 app.py plot-full-pipeline ../../extracted_features/extracted_features_OSTest/ OSTest
