0104c95ca2bce89b73dc4881200690dab2d7cc90
dlopes <dlopes@gasolina>
2023-09-28 13:28:39 +0100

    Added script to run all experiments

---
+++ experiment_all.sh
@@ -0,0 +1,32 @@
+echo "\n\n### Running setup.sh"
+./setup.sh 
+
+echo "\n\n### Running experiment1.sh"
+./experiment1.sh
+
+echo "\n\n### Running experiment1_results.sh"
+./experiment1_results.sh
+
+echo "\n\n### Running experiment2.sh"
+./experiment2.sh
+
+echo "\n\n### Running experiment3_setup.sh"
+./experiment3_setup.sh
+
+echo "\n\n### Running experiment3.sh"
+./experiment3.sh
+
+echo "\n\n### Running experiment3_results.sh"
+./experiment3_results.sh
+
+echo "\n\n### Running experiment4_setup.sh"
+./experiment4_setup.sh
+
+echo "\n\n### Running experiment4.sh"
+./experiment4.sh
+
+echo "\n\n### Running experiment5_setup.sh"
+./experiment5_setup.sh
+
+echo "\n\n### Running experiment5.sh"
+./experiment5.sh
