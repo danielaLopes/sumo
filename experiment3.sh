8eb421d98dbd8eb48b1f88ae0c6bb32ae95f5c2e
dcastro <daniel.castro@tecnico.ulisboa.pt>
2023-09-28 17:18:53 +0100

    minor changes

--- experiment3.sh
+++ experiment3.sh
@@ -1,3 +1,5 @@
+mkdir experiment3
+
 echo "Testing source separation model ..."
 cd sumo_pipeline/source_separation
 LOCATION_EXTRACTED_FEATURES=../../extracted_features
@@ -10,3 +12,5 @@ python3 app.py test-full-pipeline OSTest ../../models/target_separation/target_s
 echo "Testing session correlation with full pipeline ..."
 cd ../session_correlation
 python3 app.py correlate-sessions-full-pipeline OSTest
+
+cp results/figures_full_pipeline/*.{pdf,png} ../../experiment3
