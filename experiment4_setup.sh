ade266f9ee97cbe30173d432bd283f4b629141c5
dlopes <dlopes@gasolina>
2023-09-28 12:39:39 +0100

    [WIP] fixing paths and setup script for deepcoffea

--- experiment4_setup.sh
+++ experiment4_setup.sh
@@ -11,5 +11,14 @@ if [ ! -d "deepcoffea" ]; then
     git clone https://github.com/traffic-analysis/deepcoffea.git
 fi
 
+echo "Downloading OSTrain, OSValidate, and OSTest extracted features in DeepCoFFEA's format ..."
+curl -o sumo_features_for_deepcoffea.tar.gz -L "https://zenodo.org/record/8386335/files/sumo_features_for_deepcoffea.tar.gz?download=1"
+tar -xf sumo_features_for_deepcoffea.tar.gz
+
 echo "Converting SUMo's features into DeepCoFFEA's features ..."
 python3 sumo_to_deepcoffea_features.py
+
+# TODO: Sill producing errors
+echo "Generating model files for DeepCoFFEA ..."
+./scripts/run_dcf_20230521.sh 
+./scripts/run_dcf_20230521_2.sh 
