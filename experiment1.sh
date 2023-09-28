84c4e037d743512fb5d7c6952ddb61180b3677a3
dcastro <daniel.castro@tecnico.ulisboa.pt>
2023-09-28 22:06:09 +0100

    minor changes

--- experiment1.sh
+++ experiment1.sh
@@ -1,5 +1,6 @@
 
 cd sumo_pipeline/session_correlation
+make torpedosubsetsumopencl2d.so
 python3 app.py correlate-sessions OSTest
 
 
--- experiment_all.sh
+++ experiment_all.sh
@@ -1,37 +1,37 @@
 # Uncomment if running in personal machine. 
 # Requires sudo
 #echo "\n\n### Running setup.sh"
-#./setup.sh 
+#bash ./setup.sh 
 
 echo "\n\n### Running experiment1_setup.sh"
-./experiment1_setup.sh
+bash ./experiment1_setup.sh
 
 echo "\n\n### Running experiment1.sh"
-./experiment1.sh
+bash ./experiment1.sh
 
 echo "\n\n### Running experiment1_results.sh"
-./experiment1_results.sh
+bash ./experiment1_results.sh
 
 echo "\n\n### Running experiment2.sh"
-./experiment2.sh
+bash ./experiment2.sh
 
 echo "\n\n### Running experiment3_setup.sh"
-./experiment3_setup.sh
+bash ./experiment3_setup.sh
 
 echo "\n\n### Running experiment3.sh"
-./experiment3.sh
+bash ./experiment3.sh
 
 echo "\n\n### Running experiment3_results.sh"
-./experiment3_results.sh
+bash ./experiment3_results.sh
 
 echo "\n\n### Running experiment4_setup.sh"
-./experiment4_setup.sh
+bash ./experiment4_setup.sh
 
 echo "\n\n### Running experiment4.sh"
-./experiment4.sh
+bash ./experiment4.sh
 
 echo "\n\n### Running experiment5.sh"
-./experiment5.sh
+bash ./experiment5.sh
 
 echo "\n\n### Running experiment5_results.sh"
-./experiment5_results.sh
+bash ./experiment5_results.sh
