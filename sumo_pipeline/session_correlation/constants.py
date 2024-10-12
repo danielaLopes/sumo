61493e6fbcde09362b4cf69739040747aacff3ba
Daniel Castro <daniel_me@live.com.pt>
2024-10-12 08:11:22 +0100

    changed hard coded path to relative one, still not a definitive fix

--- sumo_pipeline/session_correlation/constants.py
+++ sumo_pipeline/session_correlation/constants.py
@@ -10,4 +10,4 @@ def get_session_concurrency_at_onion_file_name(dataset_name: str) -> str:
     return f'{DATA_RESULTS_FOLDER}os_session_concurrency_{dataset_name}.pickle'
 
 def get_captures_folder(dataset_name: str) -> str:
-    return f"/mnt/nas-shared/torpedo/extracted_features/extracted_features_{dataset_name}"
+    return f"../../extracted_features/extracted_features_{dataset_name}"
